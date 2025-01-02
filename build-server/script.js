658d089dcd3269fdf502bef218deb0020f4c1e96
bitbard3 <ansharora3839@gmail.com>
2025-01-02 16:46:15 +0530

    Upload files to S3

--- build-server/script.js
+++ build-server/script.js
@@ -1,8 +1,40 @@
 const { exec } = require("child_process")
 const path = require("path")
+const fs = require("fs")
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+const mime = require('mime-types')
+const PROJECT_ID = process.env.PROJECT_ID
+
+const s3Client = new S3Client({
+    region: process.env.S3_REGION,
+    credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY_ID,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+    }
+})
 
 
-
+const uploadFiles = async () => {
+    console.log('Build Complete')
+    const distDirPath = path.join(__dirname, 'output', 'dist')
+    const distContent = fs.readdirSync(distDirPath, { recursive: true })
+    console.log('Starting Upload')
+    for (const file of distContent) {
+        const filePath = path.join(distDirPath, file)
+        if (fs.lstatSync(filePath).isDirectory()) {
+            continue;
+        }
+        console.log('Uploading', filePath)
+        const command = new PutObjectCommand({
+            Bucket: process.env.S3_BUCKET,
+            Key: `__outputs/${PROJECT_ID}/${filePath}`,
+            Body: fs.createReadStream(filePath),
+            ContentType: mime.lookup(filePath)
+        })
+        await s3Client.send(command)
+    }
+    console.log('Done')
+}
 
 
 const init = async () => {
@@ -19,3 +51,5 @@ const init = async () => {
     })
     p.on('close', uploadFiles())
 }
+
+init()
