e0b4d959430643a5079fe6fd861141e13abe3d17
bitbard3 <ansharora3839@gmail.com>
2025-01-03 15:50:42 +0530

    Fix: Fix typo

--- build-server/script.js
+++ build-server/script.js
@@ -40,7 +40,7 @@ const uploadFiles = async () => {
 const init = async () => {
     console.log("Executing script.js")
     const outDirPath = path.join(__dirname, 'output')
-    const p = exec(`cd {outDirPath} && npm install && npm run build`)
+    const p = exec(`cd ${outDirPath} && npm install && npm run build`)
 
     p.stdout.on('data', (data) => {
         console.log(data.toString())
