1a08df5ae3982cf27d10f5dbeaff0a8778d5c8b2
bitbard3 <ansharora3839@gmail.com>
2025-01-12 02:08:55 +0530

    Add:Create Project controller

---
+++ api-server/src/controllers/app/createProject.controller.ts
@@ -0,0 +1,40 @@
+import type { RequestHandler } from "express";
+import prisma from "../../../prisma/db";
+import { createProjectSchema } from "../../validations/app/validatons";
+import { generateSlug } from "random-word-slugs";
+
+export const createProject: RequestHandler = async (req, res) => {
+  const parsedData = createProjectSchema.safeParse(req.body);
+  if (!parsedData.success) {
+    res.status(400).json({
+      message: "Invalid inputs",
+      error: parsedData.error.format(),
+    });
+    return;
+  }
+  const data = parsedData.data;
+  try {
+    const project = await prisma.project.create({
+      data: {
+        gitURL: data.gitURL,
+        name: data.name,
+        userId: req.userId,
+        subDomain: generateSlug(),
+      },
+      select: {
+        id: true,
+        subDomain: true,
+        name: true,
+      },
+    });
+    res.status(200).json({
+      project: project.id,
+      subDomain: project.subDomain,
+      name: project.name,
+    });
+    return;
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong" });
+    return;
+  }
+};
