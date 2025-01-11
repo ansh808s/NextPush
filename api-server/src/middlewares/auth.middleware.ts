2b439e2728539dfa7566f51990f3fe8c7c99da93
bitbard3 <ansharora3839@gmail.com>
2025-01-12 02:08:20 +0530

    Add:Auth middlware for protecting routes

---
+++ api-server/src/middlewares/auth.middleware.ts
@@ -0,0 +1,22 @@
+import type { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+import type { AuthToken } from "../types/auth.types";
+
+export const authMiddleware: RequestHandler = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    res.status(403).json({ msg: "Not authorizated" });
+    return;
+  }
+  try {
+    const tokenDecoded = jwt.verify(
+      token,
+      process.env.JWTSECRET || ""
+    ) as AuthToken;
+    req.userId = tokenDecoded.id;
+    next();
+  } catch (error) {
+    res.status(403).json({ msg: "Not authorizated" });
+    return;
+  }
+};
