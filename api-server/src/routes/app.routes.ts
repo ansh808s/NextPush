4dee0b8512e010c0446fa96eaa871f3e7f494782
bitbard3 <ansharora3839@gmail.com>
2025-01-12 02:11:29 +0530

    Add: /project route for creating project

--- api-server/src/routes/app.routes.ts
+++ api-server/src/routes/app.routes.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { createProject } from "../controllers/app/createProject.controller";
 
 const router = express.Router();
+
+router.post("/project", authMiddleware, createProject);
 
 export default router;
