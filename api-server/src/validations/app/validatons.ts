b4dc24c8612f19cfece5a53b0c81409b5c7925d5
bitbard3 <ansharora3839@gmail.com>
2025-01-13 07:06:16 +0530

    Add: Create deployment schema

--- api-server/src/validations/app/validatons.ts
+++ api-server/src/validations/app/validatons.ts
@@ -1,5 +1,10 @@
 import z from "zod";
+
 export const createProjectSchema = z.object({
   name: z.string().max(20, "Name shouldn't be more than 20 characters"),
   gitURL: z.string({ required_error: "Git URL is required" }),
+});
+
+export const createDeploymentSchema = z.object({
+  projectId: z.string({ required_error: "Project ID is required" }),
 });
