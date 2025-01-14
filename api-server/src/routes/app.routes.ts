adc461df0dac65fca080fea86bf076595a3e6e8d
bitbard3 <ansharora3839@gmail.com>
2025-01-14 18:37:33 +0530

    Add: Deploy route

--- api-server/src/routes/app.routes.ts
+++ api-server/src/routes/app.routes.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { createProject } from "../controllers/app/createProject.controller";
+import { createDeployment } from "../controllers/app/createDeployment.controller";
 
 const router = express.Router();
 
 router.post("/project", authMiddleware, createProject);
+router.post("/deploy", authMiddleware, createDeployment);
 
 export default router;
