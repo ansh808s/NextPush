a731586bcc375a116b3602979b3875e737a4a5ef
bitbard3 <ansharora3839@gmail.com>
2025-01-14 07:07:03 +0530

    Add: deploy task service to spin up the container

---
+++ api-server/src/services/AWS/deployTask.ts
@@ -0,0 +1,45 @@
+import { RunTaskCommand } from "@aws-sdk/client-ecs";
+import { ecsClient } from "../../../config/aws/ecsClient";
+
+interface IDeployTask {
+  gitURL: string;
+  projectId: string;
+  deploymentId: string;
+}
+
+export const deployTask = async (props: IDeployTask) => {
+  const command = new RunTaskCommand({
+    cluster: process.env.ECS_CLUSTER,
+    taskDefinition: process.env.ECS_TASK_DEFINITION,
+    launchType: "FARGATE",
+    count: 1,
+    networkConfiguration: {
+      awsvpcConfiguration: {
+        assignPublicIp: "ENABLED",
+        subnets: process.env.ECS_SUBNETS?.split(","),
+        securityGroups: process.env.ECS_SECURITY_GROUPS?.split(","),
+      },
+    },
+    overrides: {
+      containerOverrides: [
+        {
+          name: "builder-image",
+          environment: [
+            { name: "GIT_REPO_URL", value: props.gitURL },
+            { name: "PROJECT_ID", value: props.projectId },
+            { name: "S3_BUCKET", value: process.env.S3_BUCKET },
+            { name: "S3_REGION", value: process.env.S3_REGION },
+            { name: "S3_ACCESS_KEY_ID", value: process.env.S3_ACCESS_KEY_ID },
+            {
+              name: "S3_SECRET_ACCESS_KEY",
+              value: process.env.S3_SECRET_ACCESS_KEY,
+            },
+          ],
+        },
+      ],
+      cpu: "1024",
+      memory: "2048",
+    },
+  });
+  return await ecsClient.send(command);
+};
