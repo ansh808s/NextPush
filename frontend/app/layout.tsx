2b2d15ac34e34a44358ff9cab8122b5673a47602
bitbard3 <ansharora3839@gmail.com>
2025-01-20 03:43:14 +0530

    feat: Add provider wrapper to the root of layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "./theme/ThemeProvider";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import ClientProviders from "@/components/ClientProviders";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,13 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ClientProviders>
           <div className="flex flex-col min-h-screen">
             <Navbar />
             <main className="flex-grow">{children}</main>
             <Footer />
           </div>
-        </ThemeProvider>
+        </ClientProviders>
       </body>
     </html>
   );
