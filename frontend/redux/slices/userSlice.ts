b06c91d80109a9307155b888caf05b5a2843b9c1
bitbard3 <ansharora3839@gmail.com>
2025-01-20 21:20:20 +0530

    feat: Create user slice to setup state for user details

---
+++ frontend/redux/slices/userSlice.ts
@@ -0,0 +1,23 @@
+import { User } from "@/types/auth/types";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface UserState {
+  user: User | null;
+}
+
+const initialState: UserState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<User>) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
