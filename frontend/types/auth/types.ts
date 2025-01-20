c1deb259c44fa78a289fdb4bee29c128454844e2
bitbard3 <ansharora3839@gmail.com>
2025-01-20 20:46:10 +0530

    feat: Add Auth response interface for recieving data from the api

--- frontend/types/auth/types.ts
+++ frontend/types/auth/types.ts
@@ -1,3 +1,13 @@
 export type AuthToken = {
   token: string;
 };
+
+export type User = {
+  username: string;
+  avatar: string;
+  email: string;
+};
+
+export interface AuthResponse extends AuthToken {
+  data: User;
+}
