b2b7f2b9f908b864fbd5b0334c70a00cf4b73b1d
bitbard3 <ansharora3839@gmail.com>
2025-01-21 03:47:23 +0530

    refactor: Remove footer from root of project

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 import ClientProviders from "@/components/ClientProviders";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,7 +22,6 @@ export default function RootLayout({
           <div className="flex flex-col min-h-screen">
             <Navbar />
             <main className="flex-grow">{children}</main>
-            <Footer />
           </div>
         </ClientProviders>
       </body>
