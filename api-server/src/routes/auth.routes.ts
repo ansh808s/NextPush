e28f035e2bc5bab4a6f4323856c92083501208a8
bitbard3 <ansharora3839@gmail.com>
2025-01-25 09:54:37 +0530

    feat:Add /repo route to fetch repo details

--- api-server/src/routes/auth.routes.ts
+++ api-server/src/routes/auth.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { signin } from "../controllers/auth/signin.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { getRepo } from "../controllers/auth/getRepo.controller";
 
 const router = express.Router();
 
 router.post("/signin", signin);
-
+router.get("/repo", authMiddleware, getRepo);
 export default router;
