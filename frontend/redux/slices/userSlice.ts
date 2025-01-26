221fd3e17956049e767147d56f5766325114d48a
bitbard3 <ansharora3839@gmail.com>
2025-01-26 21:49:57 +0530

    feat:Add reducer to to setRepo state

--- frontend/redux/slices/userSlice.ts
+++ frontend/redux/slices/userSlice.ts
@@ -1,12 +1,14 @@
-import { User } from "@/types/auth/types";
+import { Repository, User } from "@/types/auth/types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface UserState {
   user: User | null;
+  repo: Repository[];
 }
 
 const initialState: UserState = {
   user: null,
+  repo: [],
 };
 
 const userSlice = createSlice({
@@ -16,8 +18,11 @@ const userSlice = createSlice({
     setUser: (state, action: PayloadAction<User>) => {
       state.user = action.payload;
     },
+    setRepo: (state, action: PayloadAction<Repository[]>) => {
+      state.repo = action.payload;
+    },
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setRepo } = userSlice.actions;
 export default userSlice.reducer;
