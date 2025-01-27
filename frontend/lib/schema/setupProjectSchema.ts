23f0147c0903f8a7227260db24253acd8019a844
bitbard3 <ansharora3839@gmail.com>
2025-01-27 09:59:52 +0530

    feat:Add form schema for setting up project

---
+++ frontend/lib/schema/setupProjectSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+export const formSchema = z.object({
+  projectName: z.string().min(1, "Project Name is required"),
+  framework: z.string().min(1, "Please select a framework"),
+  envVars: z
+    .array(
+      z.object({
+        key: z.string().optional(),
+        value: z.string().optional(),
+      })
+    )
+    .optional(),
+});
+
+export type FormData = z.infer<typeof formSchema>;
