a09208fa4c044fb765dee5139db7a4620fb590a3
bitbard3 <ansharora3839@gmail.com>
2025-01-27 19:35:04 +0530

    feat:Add /tree route for getting repo

--- api-server/src/routes/auth.routes.ts
+++ api-server/src/routes/auth.routes.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { signin } from "../controllers/auth/signin.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { getRepo } from "../controllers/auth/getRepo.controller";
+import { getRepoTree } from "../controllers/auth/getRepoTree.controller";
 
 const router = express.Router();
 
 router.post("/signin", signin);
 router.get("/repo", authMiddleware, getRepo);
-
+router.post("/tree", authMiddleware, getRepoTree);
 export default router;
