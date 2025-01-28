c6a5e31e989d94504ab835acbd33e3d0e8837ce1
bitbard3 <ansharora3839@gmail.com>
2025-01-29 03:07:48 +0530

    feat: add framework and rootDIr fields to project schema

--- api-server/src/validations/app/validatons.ts
+++ api-server/src/validations/app/validatons.ts
@@ -1,8 +1,18 @@
 import z from "zod";
+import { SupportedFrameworks } from "../../config/constants";
 
 export const createProjectSchema = z.object({
-  name: z.string().max(20, "Name shouldn't be more than 20 characters"),
-  gitURL: z.string({ required_error: "Git URL is required" }),
+  name: z
+    .string({ required_error: "Name is required" })
+    .max(20, "Name shouldn't be more than 20 characters"),
+  gitURL: z
+    .string({ required_error: "Git URL is required" })
+    .url({ message: "Please provide a valid URL" }),
+  framework: z.nativeEnum(SupportedFrameworks, {
+    required_error: "Framework is required",
+    invalid_type_error: "Framework is not supported",
+  }),
+  rootDir: z.string({ required_error: "Root directory is required" }),
 });
 
 export const createDeploymentSchema = z.object({
