0ecd1fd68a08418917a342c20e7b420ffdf2b4d1
bitbard3 <ansharora3839@gmail.com>
2025-01-30 10:32:56 +0530

    refactor: Clickhouse client variable

--- api-server/src/config/clickhouse/client.ts
+++ api-server/src/config/clickhouse/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@clickhouse/client";
 
-export const clickhouseClient = createClient({
+export const client = createClient({
   host: process.env.CH_URL!,
   database: process.env.CH_DB!,
   username: process.env.CH_USER!,
