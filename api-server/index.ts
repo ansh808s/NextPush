fa12d159674c7c2879f5874bf4da8ef1e3e0f0d8
bitbard3 <ansharora3839@gmail.com>
2025-01-30 09:43:13 +0530

    chore: init kafka consumer in index.ts

--- api-server/index.ts
+++ api-server/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./src/routes/routes";
+import { initkafkaConsumer } from "./src/config/kafka/client";
 
 const port = 9000;
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
 app.use("/api", router);
+
+initkafkaConsumer();
 
 app.listen(port, () => {
   console.log(`Running in port ${port}`);
