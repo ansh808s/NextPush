7cfde435fbe824eccabfe13135b85fcd103588ba
bitbard3 <ansharora3839@gmail.com>
2025-01-30 09:41:24 +0530

    feat: add /app/logs route to fetch route

--- api-server/src/routes/app.routes.ts
+++ api-server/src/routes/app.routes.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { createProject } from "../controllers/app/createProject.controller";
 import { createDeployment } from "../controllers/app/createDeployment.controller";
+import { getDeploymentLogs } from "../controllers/app/getDeploymentLogs.controller";
 
 const router = express.Router();
 
 router.post("/project", authMiddleware, createProject);
 router.post("/deploy", authMiddleware, createDeployment);
+router.get("/logs/:id", authMiddleware, getDeploymentLogs);
 
 export default router;
