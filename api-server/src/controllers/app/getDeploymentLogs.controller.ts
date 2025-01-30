1cf4558fd363e1d78149efca187c2e911fd858a6
bitbard3 <ansharora3839@gmail.com>
2025-01-30 09:41:05 +0530

    feat: add controller to fetch logs

---
+++ api-server/src/controllers/app/getDeploymentLogs.controller.ts
@@ -0,0 +1,16 @@
+import type { RequestHandler } from "express";
+import { client } from "../../config/clickhouse/client";
+
+export const getDeploymentLogs: RequestHandler = async (req, res) => {
+  const id = req.params.id;
+  const logs = await client.query({
+    query: `SELECT event_id, deployment_id, log, timestamp from log_events where deployment_id = {deployment_id:String}`,
+    query_params: {
+      deployment_id: id,
+    },
+    format: "JSONEachRow",
+  });
+  const rawLogs = await logs.json();
+  res.status(200).json({ logs: rawLogs });
+  return;
+};
