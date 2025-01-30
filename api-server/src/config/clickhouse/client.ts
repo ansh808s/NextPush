009c2b809123fd652333f842446a575bc6066948
bitbard3 <ansharora3839@gmail.com>
2025-01-30 10:49:57 +0530

    chore: Update depreceated key in clickhouse client

--- api-server/src/config/clickhouse/client.ts
+++ api-server/src/config/clickhouse/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@clickhouse/client";
 
 export const client = createClient({
-  host: process.env.CH_URL!,
+  url: process.env.CH_URL!,
   database: process.env.CH_DB!,
   username: process.env.CH_USER!,
   password: process.env.CH_PASS!,
