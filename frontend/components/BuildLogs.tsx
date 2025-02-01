6ee5f7f4c23f851a7108e6d4288d935799638473
bitbard3 <ansharora3839@gmail.com>
2025-02-01 06:09:53 +0530

    feat: Add BuildLogs component

---
+++ frontend/components/BuildLogs.tsx
@@ -0,0 +1,50 @@
+import type React from "react";
+import { useEffect, useRef } from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Rocket } from "lucide-react";
+import { LogEntry } from "@/types/app/types";
+import LogItem from "./LogItem";
+
+interface IBuildLogs {
+  isDeployed: boolean;
+  logs: Pick<LogEntry, "type" | "timestamp" | "message">[];
+  isLoadingLogs: boolean;
+}
+
+export default function BuildLogs(props: IBuildLogs) {
+  const scrollAreaRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (scrollAreaRef.current) {
+      scrollAreaRef.current.scrollTop = scrollAreaRef.current.scrollHeight;
+    }
+  }, [scrollAreaRef]);
+  return (
+    <Card className="mt-8 border-rose-200 dark:border-rose-800">
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold">
+          {props.isDeployed ? "Build Logs" : "Deployment Console"}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        {props.isDeployed ? (
+          <ScrollArea
+            className="h-[600px] w-full rounded-md border p-4"
+            ref={scrollAreaRef}
+          >
+            {props.logs.map((log, index) => (
+              <LogItem key={index} {...log} />
+            ))}
+          </ScrollArea>
+        ) : (
+          <div className="h-[600px] w-full rounded-md border p-4 flex flex-col items-center justify-center space-y-4">
+            <Rocket className="w-16 h-16 text-rose-500 " />
+            <p className="text-center text-light">
+              Once deployment is started you will see its progress here!
+            </p>
+          </div>
+        )}
+      </CardContent>
+    </Card>
+  );
+}
