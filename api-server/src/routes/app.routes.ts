45554c9c6cb5ec8f2449994c05de82f66481064e
bitbard3 <ansharora3839@gmail.com>
2025-02-02 03:55:07 +0530

    feat: Add route to get project info

--- api-server/src/routes/app.routes.ts
+++ api-server/src/routes/app.routes.ts
@@ -3,11 +3,12 @@ import { authMiddleware } from "../middlewares/auth.middleware";
 import { createProject } from "../controllers/app/createProject.controller";
 import { createDeployment } from "../controllers/app/createDeployment.controller";
 import { getDeploymentLogs } from "../controllers/app/getDeploymentLogs.controller";
+import { getProject } from "../controllers/app/getProject.controller";
 
 const router = express.Router();
 
 router.post("/project", authMiddleware, createProject);
 router.post("/deploy", authMiddleware, createDeployment);
 router.get("/logs/:id", authMiddleware, getDeploymentLogs);
-
+router.get("/project/:id", authMiddleware, getProject);
 export default router;
