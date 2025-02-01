fcb120ced10c65a9400c35fa2b30d1fdfb29cb9f
bitbard3 <ansharora3839@gmail.com>
2025-02-01 06:11:26 +0530

    feat: Add app Api slice to redux store

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -1,13 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import { userApiSlice } from "./api/userApiSlice";
+import { appApiSLice } from "./api/appApiSlice";
 export const store = configureStore({
   reducer: {
     user: userReducer,
     [userApiSlice.reducerPath]: userApiSlice.reducer,
+    [appApiSLice.reducerPath]: appApiSLice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApiSlice.middleware),
+    getDefaultMiddleware().concat(
+      userApiSlice.middleware,
+      appApiSLice.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
