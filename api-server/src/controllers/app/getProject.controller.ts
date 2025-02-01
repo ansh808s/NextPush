d5abe85cac1e704798f451c7f324b056e1061a87
bitbard3 <ansharora3839@gmail.com>
2025-02-01 06:10:51 +0530

    feat: Add controller to fetch project details

---
+++ api-server/src/controllers/app/getProject.controller.ts
@@ -0,0 +1,33 @@
+import type { RequestHandler } from "express";
+import prisma from "../../../prisma/db";
+
+export const getProject: RequestHandler = async (req, res) => {
+  const projectId = req.params.id;
+  const userId = req.userId;
+  if (!projectId) {
+    res.status(400).json({
+      message: "Project id not provided",
+    });
+    return;
+  }
+  try {
+    const user = await prisma.project.findUnique({
+      where: {
+        id: projectId,
+        user: {
+          id: userId,
+        },
+      },
+      include: {
+        Deployment: {
+          select: {
+            status: true,
+            createdAt: true,
+            id: true,
+          },
+        },
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {}
+};
