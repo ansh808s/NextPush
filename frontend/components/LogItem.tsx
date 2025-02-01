dbceeb2c8e1eda0eee77d25d84e2ba64089118e3
bitbard3 <ansharora3839@gmail.com>
2025-02-01 06:09:26 +0530

    feat: Add LogItem component

---
+++ frontend/components/LogItem.tsx
@@ -0,0 +1,48 @@
+import { LogEntry } from "@/types/app/types";
+import { CheckCircle, CircleAlert, Info, XCircle } from "lucide-react";
+
+export default function LogItem(
+  props: Pick<LogEntry, "type" | "timestamp" | "message">
+) {
+  const getIcon = () => {
+    switch (props.type) {
+      case "error":
+        return <XCircle className="w-4 h-4 text-red-500" />;
+      case "success":
+        return <CheckCircle className="w-4 h-4 text-green-500" />;
+      case "info":
+        return <Info className="w-4 h-4 text-blue-500" />;
+      case "warning":
+        return <CircleAlert className="w-4 h-4 text-yellow-500" />;
+      default:
+        return <Info className="w-4 h-4 text-blue-500" />;
+    }
+  };
+
+  const getTextColor = () => {
+    switch (props.type) {
+      case "error":
+        return "text-red-500";
+      case "success":
+        return "text-green-500";
+      case "info":
+        return "text-blue-500";
+      case "warning":
+        return "text-yellow-500";
+      default:
+        return "text-blue-500";
+    }
+  };
+
+  return (
+    <div className="flex items-start space-x-2 py-1">
+      <div className="flex-shrink-0 pt-1">{getIcon()}</div>
+      <div className="flex-grow">
+        <span className="text-sm font-medium text-gray-400">
+          {new Date(props.timestamp).toString()}
+        </span>
+        <p className={`text-sm ${getTextColor()}`}>{props.message}</p>
+      </div>
+    </div>
+  );
+}
