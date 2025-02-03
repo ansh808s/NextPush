fb0056ad241f126061ed911f978b727f525be654
bitbard3 <ansharora3839@gmail.com>
2025-02-03 12:03:54 +0530

    fix: Improve error handling in getDeploymentlogs controller

--- api-server/src/controllers/app/getDeploymentLogs.controller.ts
+++ api-server/src/controllers/app/getDeploymentLogs.controller.ts
@@ -9,22 +9,28 @@ export const getDeploymentLogs: RequestHandler = async (req, res) => {
     });
     return;
   }
-  const logs = await client.query({
-    query: `  SELECT 
-        event_id,
-        deployment_id,
-        type,
-        message,
-        timestamp
-      FROM log_events 
-      WHERE deployment_id = {deployment_id:String}
-      `,
-    query_params: {
-      deployment_id: id,
-    },
-    format: "JSONEachRow",
-  });
-  const rawLogs = await logs.json();
-  res.status(200).json({ logs: rawLogs });
-  return;
+  try {
+    const logs = await client.query({
+      query: `  SELECT 
+          event_id,
+          deployment_id,
+          type,
+          message,
+          timestamp
+        FROM log_events 
+        WHERE deployment_id = {deployment_id:String}
+        `,
+      query_params: {
+        deployment_id: id,
+      },
+      format: "JSONEachRow",
+    });
+    const rawLogs = await logs.json();
+    res.status(200).json({ logs: rawLogs });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Something went wrong" });
+    return;
+  }
 };
