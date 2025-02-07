2580bcc99d84bbb68ed8f91612171ee0af11850d
bitbard3 <ansharora3839@gmail.com>
2025-02-07 09:01:29 +0530

    feat: produce logs on site visit

--- reverse-proxy-server/index.ts
+++ reverse-proxy-server/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import type { UserSiteAnalyticsEvent } from "./types/analytics/types";
+import { sendUserSiteAnalyticsEvent } from "./config/kafka/producer";
+import { v4 as uuidv4 } from "uuid";
+import { shouldTrackPath } from "./utils/pathFilter";
 
 const app = express();
 const PORT = 8000;
 const S3_URI = process.env.S3_URI;
 
+async function logAnalytics(req: express.Request) {
+  if (!shouldTrackPath(req.path)) {
+    return;
+  }
+  const event: UserSiteAnalyticsEvent = {
+    eventId: uuidv4(),
+    siteSlug: req.hostname.split(".")[0],
+    eventType: "pageview",
+    hostname: req.hostname,
+    path: req.path,
+  };
+
+  await sendUserSiteAnalyticsEvent(event);
+}
+
 app.use((req, res) => {
+  logAnalytics(req).catch(console.error);
   const hostname = req.hostname;
   const subdomain = hostname.split(".")[0];
   const resolvesTo = `${S3_URI}/${subdomain}`;
