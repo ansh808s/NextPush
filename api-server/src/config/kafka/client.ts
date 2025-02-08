05c21614e8e21a12c1c9d471f5d7ad1f5ceff8fb
bitbard3 <ansharora3839@gmail.com>
2025-02-08 09:02:37 +0530

    feat: Add consumer to post analytics logs

--- api-server/src/config/kafka/client.ts
+++ api-server/src/config/kafka/client.ts
@@ -1,12 +1,13 @@
 import { Kafka, type Consumer } from "kafkajs";
 import { client } from "../clickhouse/client";
 import { v4 } from "uuid";
-import type { LogEvent } from "../../types/app.types";
+import type { LogEvent, UserSiteAnalyticsEvent } from "../../types/app.types";
 import prisma from "../../../prisma/db";
 
 class KafkaConsumerManager {
   private kafka: Kafka;
-  private consumer: Consumer;
+  private logConsumer: Consumer;
+  private analyticsConsumer: Consumer;
   private isConnected: boolean = false;
   private heartbeatInterval: NodeJS.Timer | null = null;
   private readonly HEARTBEAT_INTERVAL = 3000;
@@ -23,8 +24,14 @@ class KafkaConsumerManager {
       },
     });
 
-    this.consumer = this.kafka.consumer({
+    this.logConsumer = this.kafka.consumer({
       groupId: "api-server-logs-consumer",
+      sessionTimeout: 30000,
+      heartbeatInterval: 3000,
+    });
+
+    this.analyticsConsumer = this.kafka.consumer({
+      groupId: "analytics-consumer-group",
       sessionTimeout: 30000,
       heartbeatInterval: 3000,
     });
@@ -33,17 +40,202 @@ class KafkaConsumerManager {
   }
 
   private setupErrorHandlers() {
-    this.consumer.on("consumer.crash", async (event) => {
-      console.error("Consumer crashed:", event);
-      this.isConnected = false;
-      await this.handleCrash();
-    });
+    [this.logConsumer, this.analyticsConsumer].forEach((consumer) => {
+      consumer.on("consumer.crash", async (event) => {
+        console.error("Consumer crashed:", event);
+        this.isConnected = false;
+        await this.handleCrash();
+      });
 
-    this.consumer.on("consumer.disconnect", async () => {
-      console.log("Consumer disconnected");
-      this.isConnected = false;
-      await this.handleDisconnect();
+      consumer.on("consumer.disconnect", async () => {
+        console.log("Consumer disconnected");
+        this.isConnected = false;
+        await this.handleDisconnect();
+      });
     });
+  }
+  private async findProjectByNameAndSubdomain(
+    slug: string
+  ): Promise<string | null> {
+    try {
+      const project = await prisma.project.findFirst({
+        where: {
+          slug,
+        },
+        select: { id: true },
+      });
+
+      return project?.id || null;
+    } catch (error) {
+      console.error("Error finding project:", error);
+      return null;
+    }
+  }
+  private async processAnalyticsMessage(message: any) {
+    if (!message.value) return;
+
+    try {
+      const stringMessage = message.value.toString();
+      const event: UserSiteAnalyticsEvent = JSON.parse(stringMessage);
+      const projectId = await this.findProjectByNameAndSubdomain(
+        event.siteSlug
+      );
+
+      if (!projectId) {
+        console.error(`No project found for name_subdomain: ${event.siteSlug}`);
+        return;
+      }
+      console.log({
+        type: event.eventId,
+        site_id: event.siteSlug,
+      });
+
+      await prisma.userSiteAnalytics.create({
+        data: {
+          id: event.eventId,
+          eventType: event.eventType,
+          hostname: event.hostname,
+          path: event.path,
+          projectId,
+        },
+      });
+    } catch (error) {
+      console.error("Error processing analytics message:", error);
+      throw error;
+    }
+  }
+
+  private async processLogMessage(message: any) {
+    if (!message.value) return;
+
+    try {
+      const stringMessage = message.value.toString();
+      const logEvent: LogEvent = JSON.parse(stringMessage);
+
+      console.log({
+        type: logEvent.type,
+        message: logEvent.message,
+        timestamp: logEvent.timestamp,
+      });
+
+      if (logEvent.type === "error" || logEvent.type === "success") {
+        try {
+          await prisma.deployment.update({
+            where: { id: logEvent.deployment_id },
+            data: { status: logEvent.type === "error" ? "FAILED" : "READY" },
+          });
+        } catch (error) {
+          console.error(
+            `Error updating deployment (${logEvent.type} type):`,
+            error
+          );
+        }
+      }
+
+      const { query_id } = await client.insert({
+        table: "log_events",
+        values: [
+          {
+            event_id: v4(),
+            deployment_id: logEvent.deployment_id,
+            type: logEvent.type,
+            message: logEvent.message,
+            timestamp: logEvent.timestamp,
+          },
+        ],
+        format: "JSONEachRow",
+      });
+
+      return query_id;
+    } catch (error) {
+      console.error("Error processing log message:", error);
+      throw error;
+    }
+  }
+
+  public async initConsumer() {
+    try {
+      await this.logConsumer.connect();
+      await this.logConsumer.subscribe({
+        topics: ["container-logs"],
+        fromBeginning: true,
+      });
+
+      await this.analyticsConsumer.connect();
+      await this.analyticsConsumer.subscribe({
+        topics: ["site-analytics"],
+        fromBeginning: true,
+      });
+
+      this.isConnected = true;
+      this.startHeartbeat();
+
+      await this.logConsumer.run({
+        autoCommit: false,
+        eachBatch: async ({
+          batch,
+          resolveOffset,
+          heartbeat,
+          commitOffsetsIfNecessary,
+        }) => {
+          const messages = batch.messages;
+          console.log(`Received ${messages.length} log messages`);
+
+          for (const message of messages) {
+            try {
+              await this.processLogMessage(message);
+              resolveOffset(message.offset);
+              await commitOffsetsIfNecessary();
+              await heartbeat();
+            } catch (error) {
+              console.error("Error in log batch processing:", error);
+            }
+          }
+        },
+      });
+
+      await this.analyticsConsumer.run({
+        autoCommit: false,
+        eachBatch: async ({
+          batch,
+          resolveOffset,
+          heartbeat,
+          commitOffsetsIfNecessary,
+        }) => {
+          const messages = batch.messages;
+          console.log(`Received ${messages.length} analytics messages`);
+
+          for (const message of messages) {
+            try {
+              await this.processAnalyticsMessage(message);
+              resolveOffset(message.offset);
+              await commitOffsetsIfNecessary();
+              await heartbeat();
+            } catch (error) {
+              console.error("Error in analytics batch processing:", error);
+            }
+          }
+        },
+      });
+    } catch (error) {
+      console.error("Error initializing consumer:", error);
+      this.isConnected = false;
+      // @ts-ignore
+      if (error.code === "ECONNRESET") {
+        await this.handleDisconnect();
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  public async shutdown() {
+    this.isConnected = false;
+    await this.stopHeartbeat();
+    await Promise.all([
+      this.logConsumer.disconnect(),
+      this.analyticsConsumer.disconnect(),
+    ]);
   }
 
   private async handleCrash() {
@@ -71,7 +263,10 @@ class KafkaConsumerManager {
   private async reconnect() {
     try {
       if (this.isConnected) {
-        await this.consumer.disconnect();
+        await Promise.all([
+          this.logConsumer.disconnect(),
+          this.analyticsConsumer.disconnect(),
+        ]);
       }
       await this.initConsumer();
     } catch (error) {
@@ -103,120 +298,6 @@ class KafkaConsumerManager {
       clearInterval(this.heartbeatInterval);
       this.heartbeatInterval = null;
     }
-  }
-
-  private async processMessage(message: any) {
-    if (!message.value) return;
-
-    try {
-      const stringMessage = message.value.toString();
-      const logEvent: LogEvent = JSON.parse(stringMessage);
-
-      console.log({
-        type: logEvent.type,
-        message: logEvent.message,
-        timestamp: logEvent.timestamp,
-      });
-      if (logEvent.type === "error") {
-        try {
-          await prisma.deployment.update({
-            where: {
-              id: logEvent.deployment_id,
-            },
-            data: {
-              status: "FAILED",
-            },
-          });
-        } catch (error) {
-          console.error("Error updating deployment (error type):", error);
-        }
-      }
-      if (logEvent.type === "success") {
-        try {
-          await prisma.deployment.update({
-            where: {
-              id: logEvent.deployment_id,
-            },
-            data: {
-              status: "READY",
-            },
-          });
-        } catch (error) {
-          console.error("Error updating deployment (success type):", error);
-        }
-      }
-      const { query_id } = await client.insert({
-        table: "log_events",
-        values: [
-          {
-            event_id: v4(),
-            deployment_id: logEvent.deployment_id,
-            type: logEvent.type,
-            message: logEvent.message,
-            timestamp: logEvent.timestamp,
-          },
-        ],
-        format: "JSONEachRow",
-      });
-
-      return query_id;
-    } catch (error) {
-      console.error("Error processing message:", error);
-      throw error;
-    }
-  }
-
-  public async initConsumer() {
-    try {
-      await this.consumer.connect();
-      this.isConnected = true;
-
-      await this.consumer.subscribe({
-        topics: ["container-logs"],
-        fromBeginning: true,
-      });
-
-      this.startHeartbeat();
-
-      await this.consumer.run({
-        autoCommit: false,
-        eachBatch: async ({
-          batch,
-          resolveOffset,
-          heartbeat,
-          commitOffsetsIfNecessary,
-        }) => {
-          const messages = batch.messages;
-          console.log(`Received ${messages.length} messages`);
-
-          for (const message of messages) {
-            try {
-              await this.processMessage(message);
-              resolveOffset(message.offset);
-              await commitOffsetsIfNecessary();
-              await heartbeat();
-            } catch (error) {
-              console.error("Error in batch processing:", error);
-            }
-          }
-        },
-      });
-    } catch (error) {
-      console.error("Error initializing consumer:", error);
-      this.isConnected = false;
-      // @ts-ignore
-      if (error.code === "ECONNRESET") {
-        await this.handleDisconnect();
-      } else {
-        throw error;
-      }
-    }
-  }
-
-  public async shutdown() {
-    this.isConnected = false;
-    await this.stopHeartbeat();
-    await this.consumer.disconnect();
   }
 }
 
