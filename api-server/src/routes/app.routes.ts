210763f8bcfc14bf981bbb377a3605233b314110
bitbard3 <ansharora3839@gmail.com>
2025-02-08 08:29:33 +0530

    feat: Add routes for site visits and route visits

--- api-server/src/routes/app.routes.ts
+++ api-server/src/routes/app.routes.ts
@@ -4,6 +4,8 @@ import { createProject } from "../controllers/app/createProject.controller";
 import { createDeployment } from "../controllers/app/createDeployment.controller";
 import { getDeploymentLogs } from "../controllers/app/getDeploymentLogs.controller";
 import { getProject } from "../controllers/app/getProject.controller";
+import { getSiteVisits } from "../controllers/app/getSiteVisits.controller";
+import { getRouteVisits } from "../controllers/app/getRoutesVisits.controller";
 
 const router = express.Router();
 
@@ -11,4 +13,7 @@ router.post("/project", authMiddleware, createProject);
 router.post("/deploy", authMiddleware, createDeployment);
 router.get("/logs/:id", authMiddleware, getDeploymentLogs);
 router.get("/project/:id", authMiddleware, getProject);
+router.get("/site-visits", authMiddleware, getSiteVisits);
+router.get("/route-visits", authMiddleware, getRouteVisits);
+
 export default router;
