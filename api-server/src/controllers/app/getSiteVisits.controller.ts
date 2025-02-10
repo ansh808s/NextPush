8e158cfcd262a33a56ed6602f29b8306e51718cd
bitbard3 <ansharora3839@gmail.com>
2025-02-10 07:35:15 +0530

    feat:Get visits by days if type is week

--- api-server/src/controllers/app/getSiteVisits.controller.ts
+++ api-server/src/controllers/app/getSiteVisits.controller.ts
@@ -1,6 +1,7 @@
 import type { RequestHandler } from "express";
 import prisma from "../../../prisma/db";
 import { startOfDay, endOfDay, startOfWeek, endOfWeek } from "date-fns";
+import type { DayVisits } from "../../types/app.types";
 
 export const getSiteVisits: RequestHandler = async (req, res) => {
   const { id, type } = req.query;
@@ -11,15 +12,42 @@ export const getSiteVisits: RequestHandler = async (req, res) => {
     return;
   }
   const date = new Date();
-  const visits = await prisma.userSiteAnalytics.count({
-    where: {
-      projectId: id,
-      createdAt: {
-        gte: type === "today" ? startOfDay(date) : startOfWeek(date),
-        lte: type === "today" ? endOfDay(date) : endOfWeek(date),
+
+  if (type == "today") {
+    const visits = await prisma.userSiteAnalytics.count({
+      where: {
+        projectId: id,
+        createdAt: {
+          gte: startOfDay(date),
+          lte: endOfDay(date),
+        },
       },
-    },
-  });
-  res.status(200).json({ visits });
-  return;
+    });
+    res.status(200).json({ visits });
+    return;
+  } else if (type == "week") {
+    const visits: DayVisits[] = await prisma.$queryRaw`
+      SELECT 
+        DATE(created_at) as date,
+        COUNT(*) as count
+      FROM "UserSiteAnalytics"
+      WHERE project_id = ${id}
+        AND created_at >= ${startOfWeek(date)}
+        AND created_at <= ${endOfWeek(date)}
+      GROUP BY DATE(created_at)
+      ORDER BY date ASC
+    `;
+    res.status(200).json({
+      visits: visits.map((visit) => ({
+        date: visit.date.toLocaleDateString("en-US", { weekday: "long" }),
+        count: Number(visit.count),
+      })),
+    });
+    return;
+  } else {
+    res.status(400).json({
+      message: "Invalid inputs",
+    });
+    return;
+  }
 };
