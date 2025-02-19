317fee3f59afc078c86d7772ae1b80a7e18d8944
bitbard3 <ansharora3839@gmail.com>
2025-02-20 05:18:39 +0530

    Add menu for smaller screen

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -10,24 +10,27 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { ChevronDown, Plus, Layers } from "lucide-react";
+import { ChevronDown, Menu, Plus, Layers } from "lucide-react";
 import { usePathname } from "next/navigation";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useState } from "react";
 
 export default function Navbar() {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const isActive = (path: string) => {
     return pathname === path;
   };
 
   return (
-    <header className="border-b">
+    <header className="border-b flex">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold text-primary">
           Ship-it
         </Link>
 
-        <nav>
+        <nav className="md:block hidden">
           <ul className="flex space-x-4 items-center">
             <li>
               <Link
@@ -78,6 +81,45 @@ export default function Navbar() {
             </li>
           </ul>
         </nav>
+      </div>
+      <div className="flex items-center gap-4 mr-3">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon" className="md:hidden">
+              <Menu className="!h-7 !w-7" />
+              <span className="sr-only">Toggle menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="w-[300px] sm:w-[400px]">
+            <nav className="flex flex-col gap-4 mt-8">
+              <Link
+                href="/projects"
+                className="flex items-center gap-2"
+                onClick={() => setIsOpen(false)}
+              >
+                View All Projects
+              </Link>
+              <Link
+                href="/import-repository"
+                className="flex items-center gap-2"
+                onClick={() => setIsOpen(false)}
+              >
+                Create New Project
+              </Link>
+              <Link
+                href="/pricing"
+                className="flex items-center gap-2"
+                onClick={() => setIsOpen(false)}
+              >
+                Pricing
+              </Link>
+              <div className="h-px bg-gray-200 dark:bg-gray-800 my-4" />
+              <button className="flex items-center gap-2 text-left">
+                Logout
+              </button>
+            </nav>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
