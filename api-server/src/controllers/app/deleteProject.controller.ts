0697f08128fb968dfa9fddf6c592fdfdffc2dc9e
bitbard3 <ansharora3839@gmail.com>
2025-02-21 05:49:19 +0530

    Add delete project controller

---
+++ api-server/src/controllers/app/deleteProject.controller.ts
@@ -0,0 +1,24 @@
+import type { RequestHandler } from "express";
+import prisma from "../../../prisma/db";
+
+export const deleteProject: RequestHandler = async (req, res) => {
+  const id = req.params.id;
+  if (!id) {
+    res.status(400).json({
+      message: "Project id not provided",
+    });
+    return;
+  }
+  try {
+    const project = await prisma.project.delete({
+      where: { id },
+    });
+    res.status(200).json({
+      message: `Project deleted - ${project.name}`,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong" });
+    console.log(error);
+    return;
+  }
+};
