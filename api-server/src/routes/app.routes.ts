e71fa29477f5f0ec20c1b03b8565be17849742ab
bitbard3 <ansharora3839@gmail.com>
2025-02-22 05:49:35 +0530

    Add delete project route

--- api-server/src/routes/app.routes.ts
+++ api-server/src/routes/app.routes.ts
@@ -7,6 +7,7 @@ import { getProject } from "../controllers/app/getProject.controller";
 import { getSiteVisits } from "../controllers/app/getSiteVisits.controller";
 import { getRouteVisits } from "../controllers/app/getRoutesVisits.controller";
 import { getUsererojects } from "../controllers/app/getUserProjects.controller";
+import { deleteProject } from "../controllers/app/deleteProject.controller";
 
 const router = express.Router();
 
@@ -17,5 +18,6 @@ router.get("/project/:id", authMiddleware, getProject);
 router.get("/site-visits", authMiddleware, getSiteVisits);
 router.get("/route-visits", authMiddleware, getRouteVisits);
 router.get("/project", authMiddleware, getUsererojects);
+router.delete("/project/:id", authMiddleware, deleteProject);
 
 export default router;
