0343c80d14fe2a4cda080131f81c4f8ba3e1780d
bitbard3 <ansharora3839@gmail.com>
2025-02-26 13:59:32 +0530

    Add verify route

--- api-server/src/routes/auth.routes.ts
+++ api-server/src/routes/auth.routes.ts
@@ -3,10 +3,12 @@ import { signin } from "../controllers/auth/signin.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { getRepo } from "../controllers/auth/getRepo.controller";
 import { getRepoTree } from "../controllers/auth/getRepoTree.controller";
+import { verify } from "../controllers/auth/verify.controller";
 
 const router = express.Router();
 
 router.post("/signin", signin);
 router.get("/repo", authMiddleware, getRepo);
 router.post("/tree", authMiddleware, getRepoTree);
+router.get("/verify", verify);
 export default router;
